c9793cb719b72c6aa6fc0da445f3bf6cf2f687da
Van Luu <valu4298@gmail.com>
2017-04-08 23:03:22 -0500

    init project

---
+++ Mah Income/Mah Income/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mah Income
+//
+//  Created by Van Luu on 4/8/17.
+//  Copyright © 2017 Van Luu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
